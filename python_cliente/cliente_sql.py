ea792cd406089ca197e2c60d0b2a95e8e5d8ca1b
Alexandre Fonseca <ap.fonseca86@gmail.com>
2020-02-24 00:35:03 -0300

    Atualização de banco de dados e parametros a serem armazenados (atualização da tabela)

--- python_cliente/cliente_sql.py
+++ python_cliente/cliente_sql.py
@@ -1,37 +1,36 @@
-# Essa linha é para reconhecimento de acentos (pt-br) This Python file uses the following encoding: utf-8
+# Essa linha é para reconhecimento de acentos (pt-br) This Python file uses the following encoding: utf-8 
 # Programa em Python criado para ser um cliente MQTT no broker
-# Ele assina o tópico esp/vazao e ao receber mensagens salva no arquivo banco
-# - 13/09/2019  - Versão inicial
+# Ele assina o tópico esp/vazao e ao receber mensagens salva no arquivo registro.txt
+# - 13/09/2019 	- Versão inicial
 #               - Comentários
 #               - Cria o arquivo caso nao exista (registro.txt)
 # - 15/09/2019  - Alterado método de abertura do arquivo de registro (append)
 #               - Se o arquivo não existir cria novo ('a+')
-# - 17/11/2019  - Mudança para armazenamento das mensagens para banco de dados (MySQL)
-#				- Banco funcional
+# - 17/11/2019  - mudança para armazenamento das mensagens em banco de dados (MySQL)
+# - 20/02/2020 - Mudanças nos bancos
 #
 #
 #
-#
-import MySQLdb as mdb           # importa Mysql
+import MySQLdb as mdb		# importa Mysql
 import paho.mqtt.client as mqtt # importa biblioteca Paho (Paho é uma biblioteca de MQTT para Python)
 servbroker = "localhost"
 servdb = "localhost"
 
 #chamada para conectar ao banco
-con = mdb.connect(servdb, 'aguasql', 'password', 'testepy')
+con = mdb.connect(servdb, 'aguasql', 'password', 'simcona')
 cur = con.cursor()
 
 # Chamada (função) para quando o cliente recebe a resposta CONNACK do servidor (indica que o cliente está conectado)
 def on_connect(client, userdata, flags, rc):
     print("Conectado. Código: "+str(rc))
     # Subscrevendo dentro do on_connect() significa que se perdermos a conexão todas as subinscrições serão renovadas.
-    client.subscribe("esp/vazao")
+    client.subscribe("medidores/#")
 
 # Chamada (função) para quando uma mensagem PUBLISH é recebida do servidor (broker).
 def on_message(client, userdata, msg):
     print(msg.topic+" = > "+str(msg.payload))           # Imprime na tela (console a mensagem)
-    conteudo = (msg.topic+"|"+str(msg.payload)+"\n")    # Recebe o tópico e a mensagem, concatenando em 'conteudo'
-    cur.execute("INSERT INTO registromensagens(topico, mensagem) VALUES('"+msg.topic+"','"+str(msg.payload)+"')") # prepara insert
+    conteudo = (msg.topic+"|"+str(msg.payload)+"\n")    # Recebe o tópico e a mensagem, concatenando em 'conteudo' 
+    cur.execute("INSERT INTO Registro(horario,topicoMedidor, valor) VALUES(now(),'"+msg.topic+"',"+str(msg.payload)+")") # prepara insert
     con.commit() #commit da operação do insert
 
 client = mqtt.Client(client_id="PythonCliAgua", clean_session=False) # Instancia o cliente MQTT
@@ -43,4 +42,5 @@ client.connect(servbroker, 1883, 60) # Parâmetros de conexao (hostname/ip , por
 
 # Loop principal que mantem o cliente conectado e recebendo as mensagens
 # Existem outros tipos de loop no Paho (suporte a 'threads', etc)
-client.loop_forever()
+client.loop_forever() 
+
